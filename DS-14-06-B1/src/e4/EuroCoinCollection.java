d23f2a27936db34739b699ddf67524fe6cee8c02
iker.gcalvino <iker.gcalvino@bc8669af-4b49-4684-8edd-578574d3e052>
2019-10-11 19:08:09 +0000

    Comentarios

--- DS-14-06-B1/src/e4/EuroCoinCollection.java
+++ DS-14-06-B1/src/e4/EuroCoinCollection.java
@@ -2,10 +2,14 @@ package e4;
 
 import java.util.ArrayList;
 
+// Represents a Euro coin collection
 public class EuroCoinCollection {
 
 	ArrayList < EuroCoin > coinCollection = new ArrayList();
 
+        // Inserts a coin in the collection . If the coin is already in the
+        // collection ( there is an equal coin inserted ) then the coin is not inserted .
+        // Returns true only if a new coin has been inserted in the collection .
 	public boolean insertCoin(EuroCoin coin) {
 		if (hasCoin(coin)) {
 			return false;
@@ -15,10 +19,12 @@ public class EuroCoinCollection {
 		}
 	}
 
+        // Checks if a coin has been already inserted in the collection
 	public boolean hasCoin(EuroCoin coin) {
 		return coinCollection.contains(coin);
 	}
 
+        // Returns the nominal value of the entire collection in euro cents .
 	public int value() {
 		int valor = 0;
 		for (int i = 0; i < coinCollection.size(); i++) {
@@ -27,10 +33,12 @@ public class EuroCoinCollection {
 		return valor;
 	}
 
+        // Counts the number of coins in the collection .
 	public int numCoins() {
 		return coinCollection.size();
 	}
 
+        // Removes the specified coin from the collection
 	public void removeCoin(EuroCoin coin) {
 		coinCollection.remove(coin);
 	}
