037b2f0cdec53f0e38b9736069a5d38c786e0e26
Íker García Calviño <72529208+ikergcalvino@users.noreply.github.com>
2019-11-15 20:18:00 +0000

    Actualización

--- DS-14-06-B2/src/e2_e3/Comparador.java
+++ DS-14-06-B2/src/e2_e3/Comparador.java
@@ -6,9 +6,19 @@ public class Comparador implements Comparator<EuroCoin>{
 
     @Override
     public int compare(EuroCoin coin1, EuroCoin coin2) {
-        int x = coin1.getValue().compareTo(coin2.getValue());
-        if (x == 0) { x = coin1.getPais().compareTo(coin2.getPais());
-            if (x == 0) { return coin1.getDiseno().compareTo(coin2.getDiseno()); }
+        int x = coin1.getPais().compareTo(coin2.getPais());
+        if (x == 0) { x = coin1.getValue().compareTo(coin2.getValue());
+            if (x == 0) {
+                if (coin1.año == coin2.año) {
+                    x = 0;
+                }
+                if (coin1.año < coin2.año) {
+                    x = -1;
+                }
+                if (coin1.año > coin2.año) {
+                    x = +1;
+                }
+            }
         } return x;
     }
 
