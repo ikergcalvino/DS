4a08cc0f8f076899ab433f0f4de5d3b6df01c7a6
iker.gcalvino <iker.gcalvino@bc8669af-4b49-4684-8edd-578574d3e052>
2019-11-15 19:08:17 +0000

    Actualización

--- DS-14-06-B2/test/e2_e3/EuroCoinCollectionTest.java
+++ DS-14-06-B2/test/e2_e3/EuroCoinCollectionTest.java
@@ -1,143 +1,109 @@
-//package e2_e3;
-//
-//import java.util.Comparator;
-//import java.util.Iterator;
-//import org.junit.After;
-//import org.junit.AfterClass;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-//public class EuroCoinCollectionTest {
-//    
-//    public EuroCoinCollectionTest() {
-//    }
-//    
-//    @BeforeClass
-//    public static void setUpClass() {
-//    }
-//    
-//    @AfterClass
-//    public static void tearDownClass() {
-//    }
-//    
-//    @Before
-//    public void setUp() {
-//    }
-//    
-//    @After
-//    public void tearDown() {
-//    }
-//
-//    /**
-//     * Test of insertCoin method, of class EuroCoinCollection.
-//     */
+package e2_e3;
+
+import java.util.Comparator;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.Iterator;
+
+public class EuroCoinCollectionTest {
+        EuroCoin e1_sp1, // Juan Carlos I coin
+             e1_sp2, // Felipe VI coin
+             e2_sp_2002,  // Juan Carlos I coin of 2002
+             e2_sp_2005,  // Juan Carlos I coin of 2005
+             c50_it, 
+             c20_fr, 
+             c1_pt;
+    EuroCoinCollection collection;
+    
+    Comparator<EuroCoin> comparer;
+    
+    @Before
+    public void setUp() {
+        collection = new EuroCoinCollection();
+        
+        // THIS COINS MUST BE PROPERLY CREATED IN ORDER TO THE TEST TO WORK CORRECTLY
+        e1_sp1 = new EuroCoin(EuroCoin.Valor.E1, "Juan Carlos I", EuroCoin.Pais.ES, EuroCoin.Color.ORO_PLATA, 2019);
+        e1_sp2 = new EuroCoin(EuroCoin.Valor.E1, "Felipe VI", EuroCoin.Pais.ES, EuroCoin.Color.ORO_PLATA, 2019);
+        e2_sp_2002  = new EuroCoin(EuroCoin.Valor.E2, "Juan Carlos I", EuroCoin.Pais.ES, EuroCoin.Color.ORO_PLATA, 2002);
+        e2_sp_2005  = new EuroCoin(EuroCoin.Valor.E2, "Juan Carlos I", EuroCoin.Pais.ES, EuroCoin.Color.ORO_PLATA, 2005);
+        c50_it = new EuroCoin(EuroCoin.Valor.C50, " ", EuroCoin.Pais.IT, EuroCoin.Color.ORO, 2005);
+        c20_fr = new EuroCoin(EuroCoin.Valor.C20, " ", EuroCoin.Pais.FR, EuroCoin.Color.ORO, 2005);
+        c1_pt = new EuroCoin(EuroCoin.Valor.C1, " ", EuroCoin.Pais.PT, EuroCoin.Color.BRONCE, 2005);
+       
+        assertTrue(collection.insertCoin(e1_sp1));
+        assertTrue(collection.insertCoin(e1_sp2));
+        assertTrue(collection.insertCoin(e2_sp_2002));
+        assertFalse(collection.insertCoin(e2_sp_2005)); // Not inserted
+        assertTrue(collection.insertCoin(c50_it));
+        assertTrue(collection.insertCoin(c20_fr));
+        assertFalse(collection.insertCoin(c50_it)); // Not inserted        
+    }
+    
+    @Test
+    public void testNumCoins() {       
+        assertEquals(5, collection.numCoins());
+    }
+
+    @Test
+    public void testHasCoin() {
+        assertTrue(collection.hasCoin(e1_sp1));
+        assertTrue(collection.hasCoin(e2_sp_2005));
+        assertFalse(collection.hasCoin(c1_pt));
+    }
+
+    @Test
+    public void testCollectionValue() {
+        assertEquals(470, collection.value());
+    }    
+    
+    @Test
+    public void testRemoveCoin() {
+        collection.removeCoin(c50_it);
+        assertEquals(4, collection.numCoins());
+        assertEquals(420, collection.value());
+        
+        collection.removeCoin(e2_sp_2005); // removes the 2002 coin
+        assertEquals(3, collection.numCoins());
+        assertEquals(220, collection.value());
+        
+        collection.removeCoin(c1_pt); // No coin removed
+        assertEquals(3, collection.numCoins());
+        assertEquals(220, collection.value());
+    }
+    
 //    @Test
-//    public void testInsertCoin() {
-//        System.out.println("insertCoin");
-//        EuroCoin coin = null;
-//        EuroCoinCollection instance = new EuroCoinCollection();
-//        boolean expResult = false;
-//        boolean result = instance.insertCoin(coin);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
+//    public void testGetters(){
+//        assertEquals();
+//        assertEquals();
+//        assertEquals();
+//        assertEquals();
+//        assertEquals();
 //    }
-//
-//    /**
-//     * Test of hasCoin method, of class EuroCoinCollection.
-//     */
-//    @Test
-//    public void testHasCoin() {
-//        System.out.println("hasCoin");
-//        EuroCoin coin = null;
-//        EuroCoinCollection instance = new EuroCoinCollection();
-//        boolean expResult = false;
-//        boolean result = instance.hasCoin(coin);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of value method, of class EuroCoinCollection.
-//     */
-//    @Test
-//    public void testValue() {
-//        System.out.println("value");
-//        EuroCoinCollection instance = new EuroCoinCollection();
-//        int expResult = 0;
-//        int result = instance.value();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of numCoins method, of class EuroCoinCollection.
-//     */
-//    @Test
-//    public void testNumCoins() {
-//        System.out.println("numCoins");
-//        EuroCoinCollection instance = new EuroCoinCollection();
-//        int expResult = 0;
-//        int result = instance.numCoins();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of removeCoin method, of class EuroCoinCollection.
-//     */
-//    @Test
-//    public void testRemoveCoin() {
-//        System.out.println("removeCoin");
-//        EuroCoin coin = null;
-//        EuroCoinCollection instance = new EuroCoinCollection();
-//        instance.removeCoin(coin);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of ordenador1 method, of class EuroCoinCollection.
-//     */
-//    @Test
-//    public void testOrdenador1() {
-//        System.out.println("ordenador1");
-//        EuroCoinCollection instance = new EuroCoinCollection();
-//        instance.ordenador1();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of ordenador2 method, of class EuroCoinCollection.
-//     */
-//    @Test
-//    public void testOrdenador2() {
-//        System.out.println("ordenador2");
-//        Comparator<EuroCoin> comparer = null;
-//        EuroCoinCollection instance = new EuroCoinCollection();
-//        instance.ordenador2(comparer);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of iterator method, of class EuroCoinCollection.
-//     */
-//    @Test
-//    public void testIterator() {
-//        System.out.println("iterator");
-//        EuroCoinCollection instance = new EuroCoinCollection();
-//        Iterator<EuroCoin> expResult = null;
-//        Iterator<EuroCoin> result = instance.iterator();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//    
-//}
+    
+    @Test
+    public void testHashCode(){
+        assertTrue(e1_sp1.hashCode() == e1_sp1.hashCode());
+        assertTrue(e1_sp2.hashCode() == e1_sp2.hashCode());
+        assertTrue(e2_sp_2002.hashCode() == e2_sp_2002.hashCode());
+        assertTrue(e2_sp_2005.hashCode() == e2_sp_2005.hashCode());
+        assertTrue(c50_it.hashCode() == c50_it.hashCode());
+        assertTrue(c20_fr.hashCode() == c20_fr.hashCode());
+        assertTrue(c1_pt.hashCode() == c1_pt.hashCode());
+    }
+    
+    @Test
+    public void testCompareTo(){
+        assertEquals(0,e1_sp1.compareTo(e1_sp1));
+        assertEquals(-1,e1_sp2.compareTo(e2_sp_2002));
+        assertEquals(0,e2_sp_2002.compareTo(e2_sp_2005));
+        assertEquals(0,e2_sp_2005.compareTo(e2_sp_2005));
+        assertEquals(-1,c50_it.compareTo(e1_sp1));
+        assertEquals(+1,c50_it.compareTo(c20_fr));
+    }
+    
+    @Test
+    public void testOrdenador(){
+        collection.ordenador1();
+    }
+}
