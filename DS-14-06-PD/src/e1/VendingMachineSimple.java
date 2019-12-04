e126ee20f58fc80d5afdf54dcca7640056b918b3
Íker García Calviño <72529208+ikergcalvino@users.noreply.github.com>
2019-12-04 22:13:59 +0000

    Actualización

---
+++ DS-14-06-PD/src/e1/VendingMachineSimple.java
@@ -0,0 +1,27 @@
+package e1;
+
+import java.util.*;
+
+public class VendingMachineSimple implements VendingMachine {
+
+    @Override
+    public void insertProduct(String product, int price) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void insertCoin(EuroCoin e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<EuroCoin> buy(String product) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<EuroCoin> cancel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
