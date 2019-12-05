ea36dc536f7d3de63e30c44eab94a24eefe9ea20
iker.gcalvino <iker.gcalvino@bc8669af-4b49-4684-8edd-578574d3e052>
2019-12-05 10:55:54 +0000

    Actualización

--- DS-14-06-PD/src/e1/VendingMachineComplex.java
+++ DS-14-06-PD/src/e1/VendingMachineComplex.java
@@ -6,22 +6,22 @@ public class VendingMachineComplex implements VendingMachine {
 
     @Override
     public void insertProduct(String product, int price) {
-        //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void insertCoin(EuroCoin e) {
-        //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public List<EuroCoin> buy(String product) {
-        //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public List<EuroCoin> cancel() {
-        //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
