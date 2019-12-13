b383ec450545068885e98b65ece421cb8a08438a
Íker García Calviño <72529208+ikergcalvino@users.noreply.github.com>
2019-12-13 19:23:55 +0000

    Actualización

--- DS-14-06-PD/src/e1/EuroCoin.java
+++ DS-14-06-PD/src/e1/EuroCoin.java
@@ -4,6 +4,22 @@ import java.util.*;
 
 public class EuroCoin {
 
+    public enum Valor {
+        C1(1), C2(2), C5(5),
+        C10(10), C20(20), C50(50),
+        E1(100), E2(200);
+
+        private final int v;
+
+        Valor(int value) {
+            this.v = value;
+        }
+
+        public int getValor() {
+            return v;
+        }
+    }
+    
     private final Valor valor;
     
     public EuroCoin (Valor value) {
@@ -11,36 +27,30 @@ public class EuroCoin {
     }	            
 	
     public Valor getValue() { return valor; }
-        
-	public enum Valor {
-		C1(1),   C2(2),   C5(5),
-                C10(10), C20(20), C50(50),
-                E1(100), E2(200);
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.valor);
+        return hash;
+    }
 
-		private final int v;
-                
-		Valor (int value) { this.v = value; }
-                
-		public int getValor() {	return v; }
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
 
-	@Override
-	public int hashCode() {
-            int hash = 7;
-            hash = 53 * hash + Objects.hashCode(this.valor);
-            return hash;
-	}
+        if (obj == null) {
+            return false;
+        }
 
-	@Override
-	public boolean equals (Object obj) {
-            if (this == obj) { return true; }
-            
-            if (obj == null) { return false; }
-            
-            if (getClass() != obj.getClass()) { return false; }
-            
-            final EuroCoin other = (EuroCoin) obj;
-            
-            return this.valor == other.valor;
-	}
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final EuroCoin other = (EuroCoin) obj;
+
+        return this.valor == other.valor;
+    }
 }
