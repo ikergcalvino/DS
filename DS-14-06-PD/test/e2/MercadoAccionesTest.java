61970c819cdb80047bcf632dff99df9a75a95991
iker.gcalvino <iker.gcalvino@bc8669af-4b49-4684-8edd-578574d3e052>
2019-12-13 22:24:13 +0000

    Actualización

--- DS-14-06-PD/test/e2/MercadoAccionesTest.java
+++ DS-14-06-PD/test/e2/MercadoAccionesTest.java
@@ -1,122 +1,48 @@
 package e2;
 
-import java.util.Observer;
-import org.junit.After;
-import org.junit.AfterClass;
+import java.util.*;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MercadoAccionesTest {
-    
-    public MercadoAccionesTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+
+    MercadoAcciones mercado;
+
+    Accion a, b;
+
+    Cliente c, j;
+
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+
+        mercado = new MercadoAcciones();
+
+        a = new Accion("AAAA", 0, 0, 0, 0);
+        b = new Accion("xdxd", 1, 2, 3, 4);
+
+        c = new Cliente("Paquito");
+        j = new Cliente("Jeremias");
+
     }
 
-    /**
-     * Test of insertAccion method, of class MercadoAcciones.
-     */
     @Test
-    public void testInsertAccion() {
-        System.out.println("insertAccion");
-        Accion accion = null;
-        MercadoAcciones instance = new MercadoAcciones();
-        instance.insertAccion(accion);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testSetters_Getters() {
+
+        a.setCierre(1);
+        a.setMax(2);
+        a.setMin(3);
+        a.setSimbolo("xdxd");
+        a.setVolumen(4);
+
+        assertEquals(a.getClass(), b.getClass());
+        assertTrue(a.getCierre() == b.getCierre());
+        assertTrue(a.getMax() == b.getMax());
+        assertTrue(a.getMin() == b.getMin());
+        assertEquals(a.getSimbolo(), b.getSimbolo());
+        assertTrue(a.getVolumen() == b.getVolumen());
+        assertEquals("Paquito", c.getName());
+        assertEquals("Jeremias", j.getName());
     }
 
-    /**
-     * Test of deleteAccion method, of class MercadoAcciones.
-     */
-    @Test
-    public void testDeleteAccion() {
-        System.out.println("deleteAccion");
-        Accion accion = null;
-        MercadoAcciones instance = new MercadoAcciones();
-        instance.deleteAccion(accion);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of insertCliente method, of class MercadoAcciones.
-     */
-    @Test
-    public void testInsertCliente() {
-        System.out.println("insertCliente");
-        Cliente cliente = null;
-        MercadoAcciones instance = new MercadoAcciones();
-        instance.insertCliente(cliente);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of deleteCliente method, of class MercadoAcciones.
-     */
-    @Test
-    public void testDeleteCliente() {
-        System.out.println("deleteCliente");
-        Cliente cliente = null;
-        MercadoAcciones instance = new MercadoAcciones();
-        instance.deleteCliente(cliente);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of insertObserver method, of class MercadoAcciones.
-     */
-    @Test
-    public void testInsertObserver() {
-        System.out.println("insertObserver");
-        Observer observer = null;
-        MercadoAcciones instance = new MercadoAcciones();
-        instance.insertObserver(observer);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of deleteObserver method, of class MercadoAcciones.
-     */
-    @Test
-    public void testDeleteObserver() {
-        System.out.println("deleteObserver");
-        Observer observer = null;
-        MercadoAcciones instance = new MercadoAcciones();
-        instance.deleteObserver(observer);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of notification method, of class MercadoAcciones.
-     */
-    @Test
-    public void testNotification() {
-        System.out.println("notification");
-        MercadoAcciones instance = new MercadoAcciones();
-        instance.notification();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
